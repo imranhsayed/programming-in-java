cff802b52b8e4bef743395a8965d9691b015c7cd
sum-lab <sum00ncat@gmail.com>
2022-09-19 21:26:23 +0530

    Adds documentation and formatting

--- 02-inheritance/Account.java
+++ 02-inheritance/Account.java
@@ -9,23 +9,34 @@ public class Account {
         this.input = new Scanner(System.in);
     }
 
+    /**
+     * Create the account with the no. and initial balance given by the user
+     */
     public void createAccount() {
-        System.out.println("Enter your account no");
-        
+        System.out.println("\nEnter your account no");
         this.accountNo = this.input.nextInt();
+
         System.out.println("Enter your initial balance");
         this.balance = this.input.nextFloat();
+
         System.out.println("Account No: " + this.accountNo + " created. Balance: " + this.balance);
     }
 
+    /**
+     * Deposit the amount given by the user
+     */
     public void depositFunds() {
         float depositAmount = 0;
         System.out.println("Enter deposit amount");
         depositAmount = this.input.nextFloat();
+
         this.balance += depositAmount;
         System.out.println(depositAmount + " deposited.\nNew Balance: " + this.balance); 
     }
 
+    /**
+     * Display the current balance
+     */
     public void displayBalance() {
         System.out.println("Current Balance: " + this.balance);
     }
--- 02-inheritance/Current_Bank_Account.java
+++ 02-inheritance/Current_Bank_Account.java
@@ -1,10 +1,15 @@
 
 public class Current_Bank_Account extends Account{
 
+    /**
+     * Withdraw the given amount. 
+     * If the amount is more than the balance, request another amount from the user.
+     */
     public void withdrawAmount() {
         float withdrawalAmount = 0;
         System.out.println("Enter withdrawal amount");
         withdrawalAmount = this.input.nextFloat();
+
         if (withdrawalAmount > this.balance) {
             System.out.println("Insufficient funds. Try another amount.");
             this.withdrawAmount();
